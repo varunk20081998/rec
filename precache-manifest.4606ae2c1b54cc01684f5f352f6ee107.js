ccc33e644a93836f1fc597626448d31dc3fe42c1
varunk20081998 <varunk20081998@gmail.com>
2022-02-15 17:47:04 +0530

    Updates

---
+++ precache-manifest.4606ae2c1b54cc01684f5f352f6ee107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43203bc682c9c867f918b3d40312a5d",
+    "url": "/rec/index.html"
+  },
+  {
+    "revision": "52f91e7f7dc24881b572",
+    "url": "/rec/static/css/main.c61c6578.chunk.css"
+  },
+  {
+    "revision": "283308bb875a8d99895c",
+    "url": "/rec/static/js/2.ceaebe0d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rec/static/js/2.ceaebe0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f91e7f7dc24881b572",
+    "url": "/rec/static/js/main.6a60599d.chunk.js"
+  },
+  {
+    "revision": "577fd5863db3bbb5da3f",
+    "url": "/rec/static/js/runtime-main.954fc191.js"
+  }
+]);
